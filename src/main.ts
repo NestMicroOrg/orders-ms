f24c76dd16c34f9c299dae8b4007ce8fc18bc925
JP19-03 <u202014511@upc.edu.pe>
2026-02-18 17:23:31 -0500

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+
+async function bootstrap() {
+  const logger = new Logger('Orders-MS');
+
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  )
+
+  await app.listen(envs.PORT);
+  logger.log(`Application is running on port ${envs.PORT}`);
+}
+bootstrap();
